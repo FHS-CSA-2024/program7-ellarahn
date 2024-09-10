d2580fa6c6c48ca332c0adda4664bb0562b451ae
ellarahn <ellarahn040@gmail.com>
2024-09-10 10:50:57 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,9 +1,34 @@
-//import stuff here
+import java.util.Scanner;
+public class program7{
+public static void main (String[] args) {
+     
+    Scanner input = new Scanner(System.in);
+      System.out.println("Klevins: ");
+      String inputKlevins = input.nextLine();
+      
+      System.out.println ("SchruteBucks ");
+      String inputSchruteBucks = input.nextLine();
+      
+      System.out.println ("StanlyNickles: ");
+      String inputStanlyNickles = input.nextLine();
+      
+      double SchruteBucks = Double.valueOf(inputSchruteBucks);
+      double Klevins = Double.valueOf(inputKlevins);
+      double StanlyNickles = Double.valueOf(inputStanlyNickles);
+      
 
-//Your code here
+      
+      double total = SchruteBucks + Klevins /20 + StanlyNickles / 240;
+      double endtotal = Math.round(total/100)/100;
+      
+      System.out.println("New Balance istotal is ");
+      System.out.print ("total- ");
+      System.out.format ("%.2f",total);
+      System.out.println("");
+  
+      
+      }
 
-//Paste console output below:
-/*
+}
+    
 
-
-*/
